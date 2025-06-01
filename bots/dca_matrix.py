3de1ade3232c127bd224e1c035cc613a3de092e7
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-01 15:28:36 -0500

    Update dca_matrix.py

--- bots/dca_matrix.py
+++ bots/dca_matrix.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from utils.config import get_mode
 from utils.kraken_wrapper import get_live_balances, get_live_prices
-from utils.paper_reset import load_paper_balances
 from utils.trade_executor import execute_trade
 from utils.performance_logger import log_dca_trade
 from utils.firebase_db import load_firebase_json, save_firebase_json
