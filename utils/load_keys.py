6839183803e111156ada0afdfb3dbae21ed29002
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-02 21:40:40 -0500

    Update load_keys.py

--- utils/load_keys.py
+++ utils/load_keys.py
@@ -2,13 +2,16 @@ import streamlit as st
 from utils.encryption import decrypt_string
 from utils.firebase_config import firebase
 
-def load_user_api_keys(user_id, exchange, token):
-    token = st.session_state.user["token"]
+def load_user_api_keys(user_id, exchange, token=None):
+    if not token:
+        token = st.session_state.user["token"]
+
     db = firebase.database()
     result = db.child("users").child(user_id).child("api_keys").child(exchange).get(token)
+
     if result.val():
-        encrypted_key = result.val().get("key", "")
-        encrypted_secret = result.val().get("secret", "")
+        encrypted_key = result.val().get("public", "")
+        encrypted_secret = result.val().get("private", "")
         return {
             "key": decrypt_string(encrypted_key, user_id),
             "secret": decrypt_string(encrypted_secret, user_id)
@@ -16,8 +19,5 @@ def load_user_api_keys(user_id, exchange, token):
     return None
 
 def api_keys_exist(user_id):
-    """
-    Checks if decrypted API keys exist for the user.
-    """
-    keys = get_decrypted_api_keys(user_id, "kraken")
+    keys = load_user_api_keys(user_id, "kraken")
     return bool(keys and keys.get("key") and keys.get("secret"))
