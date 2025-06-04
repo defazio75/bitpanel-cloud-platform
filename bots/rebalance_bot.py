2a15dc2b74f2fbb976f9f8853ad39921a06ea61b
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-03 20:22:01 -0500

    Update rebalance_bot.py

--- bots/rebalance_bot.py
+++ bots/rebalance_bot.py
@@ -9,7 +9,7 @@ from utils.firebase_db import (
     save_portfolio_snapshot
 )
 
-def rebalance_hodl(user_id):
+def rebalance_hodl(user_id, token):
     mode = get_mode(user_id=user_id)
     print(f"[Rebalance HODL] Running in {mode.upper()} mode...")
 
