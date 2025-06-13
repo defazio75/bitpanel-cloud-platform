4c55686b2c3a48e06a9339ed6ba12da056e152eb
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-13 11:46:17 -0500

    Update bot_runner.py

--- controller/bot_runner.py
+++ controller/bot_runner.py
@@ -1,4 +1,7 @@
 # bot_runner.py
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import time
 import traceback
 from controller import controller
