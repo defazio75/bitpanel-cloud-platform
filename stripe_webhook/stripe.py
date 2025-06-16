ed8444a7f7b181b71a296c00ba1b0b72d25dff15
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-16 11:17:07 -0500

    Update stripe.py

--- stripe_webhook/stripe.py
+++ stripe_webhook/stripe.py
@@ -7,7 +7,7 @@ from firebase_admin import credentials, firestore
 
 # === Initialize Firebase ===
 if not firebase_admin._apps:
-    cred = credentials.ApplicationDefault()
+    cred = credentials.Certificate("firebase_key.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
