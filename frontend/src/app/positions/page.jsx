5d8e0a103ec064874ebcc44eb04f7a725fb92c22
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-25 14:56:04 -0500

    Create page.jsx

---
+++ frontend/src/app/positions/page.jsx
@@ -0,0 +1,8 @@
+// frontend/app/positions/page.jsx
+"use client";
+
+import Positions from "../../src/components/Positions";
+
+export default function PositionsPage() {
+  return <Positions mode="paper" />;
+}
