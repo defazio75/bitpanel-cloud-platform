2244c3e886c90e767c4270b556da6a7f0e3015c9
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-26 12:17:44 -0500

    Create page.jsx

---
+++ frontend/src/app/dashboard/settings/page.jsx
@@ -0,0 +1,8 @@
+// frontend/app/settings/page.jsx
+"use client";
+
+import Settings from "../../src/components/Settings";
+
+export default function SettingsPage() {
+  return <Settings mode="paper" />;
+}
