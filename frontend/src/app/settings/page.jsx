0a5d5c1f445e104b1e5192581f09f9ccac4836b5
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-26 12:18:30 -0500

    Delete frontend/src/app/settings/page.jsx

--- frontend/src/app/settings/page.jsx
+++
@@ -1,8 +0,0 @@
-// frontend/app/settings/page.jsx
-"use client";
-
-import Settings from "../../src/components/Settings";
-
-export default function SettingsPage() {
-  return <Settings mode="paper" />;
-}
