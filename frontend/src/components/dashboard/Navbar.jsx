88852b28662582c5b0deea4dffcb60e59350042c
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-26 14:39:20 -0500

    Update Navbar.jsx

--- frontend/src/components/dashboard/Navbar.jsx
+++ frontend/src/components/dashboard/Navbar.jsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { useAuth } from '@/hooks/useAuth';
+import { useAuth } from "@/context/AuthContext";
 import Button from '@/components/ui/Button';
 
 export default function Navbar() {
