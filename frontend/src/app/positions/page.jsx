c65bf5d8174dfcb25001955002d74f861fd7adbd
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-26 12:17:16 -0500

    Delete frontend/src/app/positions/page.jsx

--- frontend/src/app/positions/page.jsx
+++
@@ -1,8 +0,0 @@
-// frontend/app/positions/page.jsx
-"use client";
-
-import Positions from "../../src/components/Positions";
-
-export default function PositionsPage() {
-  return <Positions mode="paper" />;
-}
