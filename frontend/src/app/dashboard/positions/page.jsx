89fd79aab11d76ce914a9ac49fc24ac3a1a6d085
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-26 21:24:58 -0500

    Update page.jsx

--- frontend/src/app/dashboard/positions/page.jsx
+++ frontend/src/app/dashboard/positions/page.jsx
@@ -1,7 +1,7 @@
 // frontend/app/positions/page.jsx
 "use client";
 
-import Positions from '@/components/Positions";
+import Positions from '@/components/Positions';
 
 export default function PositionsPage() {
   return <Positions mode="paper" />;
