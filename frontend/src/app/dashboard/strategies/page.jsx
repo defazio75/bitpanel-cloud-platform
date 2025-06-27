9bebf0e0e9330026ec147380237e9815cf362250
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-26 21:25:29 -0500

    Update page.jsx

--- frontend/src/app/dashboard/strategies/page.jsx
+++ frontend/src/app/dashboard/strategies/page.jsx
@@ -1,7 +1,7 @@
 // frontend/app/strategies/page.jsx
 "use client";
 
-import StrategyPage from '@/components/strategy";
+import StrategyPage from '@/components/strategy';
 
 export default function StrategiesPage() {
   return <StrategyPage mode="paper" />;
