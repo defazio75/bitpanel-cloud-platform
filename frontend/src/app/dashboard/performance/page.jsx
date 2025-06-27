a662c47d6dd23cb1483ba4de492cc0700fb48fbe
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-26 21:24:36 -0500

    Update page.jsx

--- frontend/src/app/dashboard/performance/page.jsx
+++ frontend/src/app/dashboard/performance/page.jsx
@@ -1,7 +1,7 @@
 // frontend/app/performance/page.jsx
 "use client";
 
-import Performance from '@/components/Performance";
+import Performance from '@/components/Performance';
 
 export default function PerformancePage() {
   return <Performance mode="paper" />;
