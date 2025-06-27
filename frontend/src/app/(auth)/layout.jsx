f692c143ac50244e385430268ef9df882d440912
defazio75 <74581551+defazio75@users.noreply.github.com>
2025-06-27 14:59:35 -0500

    Create layout.jsx

---
+++ frontend/src/app/(auth)/layout.jsx
@@ -0,0 +1,12 @@
+// src/app/(auth)/layout.jsx
+'use client';
+
+export default function AuthLayout({ children }) {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 bg-white rounded-3xl shadow-xl">
+        {children}
+      </div>
+    </div>
+  );
+}
